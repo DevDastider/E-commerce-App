1238b414826243f10b2e9c7ce26e55cec5bb95cb
DevDastider <sayantangd7@gmail.com>
2024-05-06 23:55:54 +0530

    images for products functionality added

--- src/main/java/com/sgd/ecommerce/controller/ProductController.java
+++ src/main/java/com/sgd/ecommerce/controller/ProductController.java
@@ -3,11 +3,20 @@
  */
 package com.sgd.ecommerce.controller;
 
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
+import com.sgd.ecommerce.model.ImageModel;
 import com.sgd.ecommerce.model.Product;
 import com.sgd.ecommerce.service.ProductService;
 
@@ -17,13 +26,32 @@ import com.sgd.ecommerce.service.ProductService;
  *
  */
 @RestController
+@CrossOrigin("*")
 public class ProductController {
 
 	@Autowired
 	private ProductService productService;
 	
-	@PostMapping({"/addProduct"})
-	public Product addNewProduct(@RequestBody Product product) {
+	@PostMapping(value = {"/addProduct"}, consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
+	public Product addNewProduct(@RequestPart("product") Product product,
+								@RequestPart("imageFiles") MultipartFile[] files) {
+		try {
+			Set<ImageModel> images = uploadImage(files);
+			product.setProductImages(images);
+		} catch(Exception ex) {
+			ex.printStackTrace();
+		}
 		return productService.addNewProduct(product);
+	}
+	
+	public Set<ImageModel> uploadImage(MultipartFile[] multipartFiles) throws IOException{
+		Set<ImageModel> images = new HashSet<>();
+		
+		for (MultipartFile file : multipartFiles) {
+			ImageModel imageModel = new ImageModel(file.getName(), file.getContentType(), file.getBytes());
+			images.add(imageModel);
+		}
+		
+		return images;
 	}
 }
--- src/main/java/com/sgd/ecommerce/model/Product.java
+++ src/main/java/com/sgd/ecommerce/model/Product.java
@@ -3,10 +3,17 @@
  */
 package com.sgd.ecommerce.model;
 
+import java.util.Set;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 
 /**
  *
@@ -23,6 +30,17 @@ public class Product {
 	private String productDescription;
 	private Double productDiscountedPrice;
 	private Double productActualPrice;
+	
+	@ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+	@JoinTable(name = "product_images",
+			joinColumns = {
+					@JoinColumn(name = "product_id")
+			},
+			inverseJoinColumns = {
+					@JoinColumn(name = "image_id")
+			}
+	)
+	private Set<ImageModel> productImages;
 	
 	public Integer getProductNumber() {
 		return productNumber;
@@ -62,6 +80,14 @@ public class Product {
 	
 	public void setProductActualPrice(Double productActualPrice) {
 		this.productActualPrice = productActualPrice;
+	}
+
+	public Set<ImageModel> getProductImages() {
+		return productImages;
+	}
+
+	public void setProductImages(Set<ImageModel> productImages) {
+		this.productImages = productImages;
 	}
 	
 }
